2a6c3e0fe42b70e59b61fa7bf616ffd0ea32228d
Gtff <1064769205@qq.com>
2022-04-21 21:34:16 +0800

    项目基本结构2.0

---
+++ g-service/g-gateway/src/main/java/com/g/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.g.gateway;
+
+/**
+ * @Author: Gtf
+ * @Date: 2022/4/21-04-21-17:05
+ * @Description: com.g.gateway
+ * @Version: 1.0
+ */
+//@EnableDiscoveryClient
+//@SpringBootApplication
+public class GatewayApplication {
+
+//    public static void main(String[] args) {
+//        .run(GateWayApplication.class, args);
+//    }
+
+}
